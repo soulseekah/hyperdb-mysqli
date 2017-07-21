e0183381f3814bad0133a9e213e21ab4e13a19ab
Cyrus Collier <web@cyruscollier.com>
2017-07-21 13:30:02 -0400

    Added utf8mb4 support

--- db.php
+++ db.php
@@ -711,7 +711,7 @@ class hyperdb extends wpdb {
 		if ( ! isset( $collate ) )
 			$collate = $this->collate;
 
-		if ( ! in_array( strtolower( $charset ), array( 'utf8', 'latin1' ) ) )
+		if ( ! in_array( strtolower( $charset ), array( 'utf8mb4', 'utf8', 'latin1' ) ) )
 			wp_die( "$charset charset isn't supported in HyperDB for security reasons" );
 
 		if ( $this->has_cap( 'collation', $dbh ) && !empty( $charset ) ) {
